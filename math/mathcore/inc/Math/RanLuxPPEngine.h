56bf8f2093a7a90b71952748f78989143095e6d6
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2018-12-07 11:35:33 +0100

    add ranluxpp generator

---
+++ math/mathcore/inc/Math/RanLuxPPEngine.h
@@ -0,0 +1,112 @@
+// @(#)root/mathcore:$Id$
+// Author: L. Moneta Tue Aug 4 2015
+
+/**********************************************************************
+ *                                                                    *
+ * Copyright (c) 2015  LCG ROOT Math Team, CERN/PH-SFT                *
+ *                                                                    *
+ *                                                                    *
+ **********************************************************************/
+
+// random engines based on ROOT 
+
+#ifndef ROOT_Math_RanLuxPPEngine
+#define ROOT_Math_RanLuxPPEngine
+
+#include "Math/TRandomEngine.h"
+
+#include <cstdint>
+#include <vector>
+#include <string>
+
+class ranluxpp;
+
+namespace ROOT {
+
+   namespace Math {
+
+      /**
+
+         Random engine baded on version 3.3 of RanLux
+         see ..
+
+
+         @ingroup Random
+      */
+
+      
+
+
+      /**
+         RanLuxPP engine
+       */
+     class RanLuxPPEngine : public TRandomEngine {
+
+
+      public:
+
+         typedef  TRandomEngine BaseType;
+         typedef  uint64_t Result_t;
+         typedef  uint64_t StateInt_t;
+
+
+	 RanLuxPPEngine(uint64_t seed=4357, int level=0);
+
+         virtual ~RanLuxPPEngine();
+
+         void SetSeed(Result_t seed) {
+            Init(seed);
+         }
+
+         virtual double Rndm() {
+            return Rndm_impl();
+         }
+         inline double operator() () { return Rndm_impl(); }
+
+         uint32_t IntRndm() {
+            return IntRndm_impl();
+         }
+       
+         /// minimum integer taht can be generated
+         static unsigned int MinInt() { return 0; }
+         /// maximum integer taht can be generated
+         static uint64_t MaxInt() { return 0xffffffffffff; }  //  2^24 -1
+
+         static int Size() { return 9; }
+
+         
+         static std::string Name() {
+            return "RanLuxPPEngine";
+         }
+
+      protected:
+         // functions used for testing
+         
+         void SetState(const std::vector<uint64_t> & state);
+
+         void GetState(std::vector<uint64_t> & state);
+
+         void Init(uint64_t seed);
+
+
+      private:
+
+         double Rndm_impl();
+         float  Rndm_impl_float();
+         uint32_t IntRndm_impl() {return (int) Rndm_impl_float()*MaxInt(); }
+
+         /* int fLevel; */
+         /* double fX[1]; */
+
+	 ranluxpp * fRlxpp;
+
+
+      };
+
+
+   } // end namespace Math
+
+} // end namespace ROOT
+
+
+#endif /* ROOT_Math_TRandomEngines */
---
+++ math/mathcore/src/RanLuxPPEngine.cxx
@@ -0,0 +1,75 @@
+// @(#)root/mathcore:$Id$
+// Authors: L. Moneta    8/2015
+
+/**********************************************************************
+ *                                                                    *
+ * Copyright (c) 2015 , ROOT MathLib Team                             *
+ *                                                                    *
+ *                                                                    *
+ **********************************************************************/
+
+// implementation file of Mersenne-Twister engine
+//
+//
+// Created by: Lorenzo Moneta  : Tue 4 Aug 201
+//
+//
+#include "Math/RanLuxPPEngine.h"
+#include "ranluxpp.h"
+#include <cassert>
+
+
+
+namespace ROOT {
+namespace Math {
+
+   ///////RanluxPP  engine
+
+  RanLuxPPEngine::RanLuxPPEngine(uint64_t seed, int luxlevel) {
+    uint64_t p = 1024;
+    if (luxlevel==0) p = 2048;    
+    fRlxpp = new ranluxpp(seed, p); 
+   }
+
+
+  RanLuxPPEngine::~RanLuxPPEngine() {
+      if (fRlxpp) delete fRlxpp; 
+   }
+
+  /// init the generator
+  void RanLuxPPEngine::Init(uint64_t seed) {
+    if (fRlxpp) return;
+    fRlxpp->init(seed);
+      //rlxd_init(level,seed);
+   }
+
+   /// generate a random double number 
+   double RanLuxPPEngine::Rndm_impl() {
+      // generate a double random number
+     return (*fRlxpp)(static_cast<double>(1.)); 
+   }
+   /// generate a double random double number 
+   float RanLuxPPEngine::Rndm_impl_float() {
+      // could use a counter to be faster
+     return (*fRlxpp)(static_cast<float>(1.)); 
+   }
+
+   void RanLuxPPEngine::SetState(const std::vector<uint64_t> & state) {
+      assert( (int) state.size() == Size() );
+      uint64_t * s = fRlxpp->getstate();
+      std::copy(state.begin(), state.end(), s); 
+   }
+
+   void RanLuxPPEngine::GetState(std::vector<uint64_t> & state) {
+      size_t size = Size();
+      uint64_t * s = fRlxpp->getstate( );
+      state = std::vector<uint64_t>( s, s+size); 
+   }
+
+   // int RanLuxPPEngine::Size() {
+   //    return  9; 
+   // }
+      
+  
+   } // namespace Math
+} // namespace ROOT
