8bc1d41bdf8a7d7aee608ee70a797f497d1e4e20
moneta <lorenzo.moneta@cern.ch>
2018-12-11 09:48:40 +0100

    Add missing file for N=16

---
+++ math/mathcore/src/MixMaxEngineImpl16.cxx
@@ -0,0 +1,16 @@
+
+
+// define number for used to Mixmax
+#define ROOT_MM_N 16
+#include "MixMaxEngineImpl.h"
+
+// define the template instance we want to have in the librsary
+//( need to be declared as extern template in the .h file)
+namespace ROOT {
+   namespace Math {
+      template class MixMaxEngine<ROOT_MM_N,0>;
+      template class MixMaxEngine<ROOT_MM_N,5>;
+      template class MixMaxEngine<ROOT_MM_N,10>;
+   }
+}
+
