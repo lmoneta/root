e34f485eb443ba0110feba626eda62b46297f328
Jonas Rembser <jonas.rembser@cern.ch>
2021-06-08 23:20:43 +0200

    [RF] Refactor weight summing for extended term
    * introduce new `RooAbsData::sumEntriesW2()` member function to
      calculate sum of squared weights of RooDataHist or RooDataSet
    
    * new `RooAbsPdf::extendedTerm(RooAbsData const&, bool weightSquared)`
      overload to conveniently compute the extended term also outside the
      the `RooNLLVar` class (intended for the new `RooFitDriver`)
    
    * always use the faster version of the squared weights summing that was
      originally designed for the batch evaluations mode
    
    * helper function in RooAbsPdf implementation to determine is extended
      term is needed

--- roofit/roofitcore/src/RooAbsData.cxx
+++ roofit/roofitcore/src/RooAbsData.cxx
@@ -2445,4 +2445,21 @@ void RooAbsData::setGlobalObservables(RooArgSet const& globalObservables) {
     if(auto lval = dynamic_cast<RooAbsRealLValue*>(arg)) lval->setConstant(true);
     if(auto lval = dynamic_cast<RooAbsCategoryLValue*>(arg)) lval->setConstant(true);
   }
+
+////////////////////////////////////////////////////////////////////////////////
+/// Return sum of squared weights of this data.
+
+double RooAbsData::sumEntriesW2() const {
+  double sumW2;
+  const RooSpan<const double> eventWeights = getWeightBatch(0, numEntries());
+  if (eventWeights.empty()) {
+    sumW2 = numEntries() * weightSquared();
+  } else {
+    ROOT::Math::KahanSum<double, 4u> kahanWeight;
+    for (std::size_t i = 0; i < eventWeights.size(); ++i) {
+      kahanWeight.AddIndexed(eventWeights[i] * eventWeights[i], i);
+    }
+    sumW2 = kahanWeight.Sum();
+  }
+  return sumW2;
 }
