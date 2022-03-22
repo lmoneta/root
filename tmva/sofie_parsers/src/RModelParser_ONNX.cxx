e81392898fef7081546a8ac9599157fb225820a2
Neel-Shah-29 <84740927+Neel-Shah-29@users.noreply.github.com>
2022-03-22 16:14:05 +0530

    Update RModelParser_ONNX.cxx

--- tmva/sofie_parsers/src/RModelParser_ONNX.cxx
+++ tmva/sofie_parsers/src/RModelParser_ONNX.cxx
@@ -59,44 +59,7 @@ std::unique_ptr<ROperator> make_ROperator_Add(const onnx::NodeProto& nodeproto, 
 
    return op;
 }
-
-std::unique_ptr<ROperator> make_ROperator_MatMul(const onnx::NodeProto& nodeproto, const onnx::GraphProto& /*graphproto */, std::unordered_map<std::string, ETensorType>& tensor_type){
-
-   ETensorType input_type = ETensorType::UNDEFINED;
-
-   for (int i = 0; i < 2; ++i) {
-      auto input_name = nodeproto.input(i);
-      auto it = tensor_type.find(input_name);
-      if (it != tensor_type.end()){
-         // according to ONNX both inputs have same time
-         if (i == 0) input_type = it->second;
-         else
-            assert(it->second == input_type);
-      } else {
-         throw std::runtime_error("TMVA::SOFIE ONNX Parser MatMul op has input tensor" + input_name + " but its type is not yet registered");
-      }
-   }
-
-   std::unique_ptr<ROperator> op;
-
-   switch(input_type){
-   case ETensorType::FLOAT:
-      op.reset(new ROperator_MatMul<float>(nodeproto.input(0), nodeproto.input(1), nodeproto.output(0)));
-      break;
-   default:
-      throw std::runtime_error("TMVA::SOFIE - Unsupported - Operator MatMul does not yet support input type " + std::to_string(static_cast<int>(input_type)));
-   }
-
-   ETensorType output_type = (op->TypeInference({input_type}))[0];
-   auto it2 = tensor_type.find(nodeproto.output(0));
-   if (it2 == tensor_type.end()){
-      tensor_type[nodeproto.output(0)] = output_type;
-   }
-
-   return op;
-}
-
-std::unique_ptr<ROperator> make_ROperator_Transpose(const onnx::NodeProto& nodeproto, const onnx::GraphProto& /graphproto/, std::unordered_map<std::string, ETensorType>& tensor_type){
+std::unique_ptr<ROperator> make_ROperator_Transpose(const onnx::NodeProto& nodeproto, const onnx::GraphProto& /*graphproto*/, std::unordered_map<std::string, ETensorType>& tensor_type){
 
    ETensorType input_type;
 
@@ -168,6 +131,38 @@ std::unique_ptr<ROperator> make_ROperator_Relu(const onnx::NodeProto& nodeproto,
    return op;
 }
 
+std::unique_ptr<ROperator> make_ROperator_Leaky_Relu(const onnx::NodeProto& nodeproto, const onnx::GraphProto& /*graphproto */, std::unordered_map<std::string, ETensorType>& tensor_type){
+
+   ETensorType input_type;
+
+   auto input_name =  nodeproto.input(0);
+   auto it = tensor_type.find(input_name);
+   if (it != tensor_type.end()){
+      input_type = it->second;
+   }else{
+      throw std::runtime_error("TMVA::SOFIE ONNX Parser leaky relu op has input tensor" + input_name + " but its type is not yet registered");
+   }
+
+   std::unique_ptr<ROperator> op;
+
+
+   switch(input_type){
+   case ETensorType::FLOAT:
+      op.reset(new ROperator_Leaky_Relu<float>(nodeproto.input(0), nodeproto.output(0)));
+      break;
+   default:
+      throw std::runtime_error("TMVA::SOFIE - Unsupported - Operator Leaky Relu does not yet support input type " + std::to_string(static_cast<int>(input_type)));
+   }
+
+   ETensorType output_type = (op->TypeInference({input_type}))[0];
+   auto it2 = tensor_type.find(nodeproto.output(0));
+   if (it2 == tensor_type.end()){
+      tensor_type[nodeproto.output(0)] = output_type;
+   }
+
+   return op;
+}
+
 std::unique_ptr<ROperator> make_ROperator_Selu(const onnx::NodeProto& nodeproto, const onnx::GraphProto& /*graphproto */, std::unordered_map<std::string, ETensorType>& tensor_type){
 
    ETensorType input_type;
@@ -221,6 +216,78 @@ std::unique_ptr<ROperator> make_ROperator_Sigmoid(const onnx::NodeProto& nodepro
       break;
    default:
       throw std::runtime_error("TMVA::SOFIE - Unsupported - Operator Sigmoid does not yet support input type " + std::to_string(static_cast<int>(input_type)));
+   }
+
+   ETensorType output_type = (op->TypeInference({input_type}))[0];
+   auto it2 = tensor_type.find(nodeproto.output(0));
+   if (it2 == tensor_type.end()){
+      tensor_type[nodeproto.output(0)] = output_type;
+   }
+
+   return op;
+}
+
+std::unique_ptr<ROperator> make_ROperator_Softmax(const onnx::NodeProto& nodeproto, const onnx::GraphProto& /*graphproto */, std::unordered_map<std::string, ETensorType>& tensor_type){
+
+   ETensorType input_type;
+
+   auto input_name =  nodeproto.input(0);
+   auto it = tensor_type.find(input_name);
+   if (it != tensor_type.end()){
+      input_type = it->second;
+   }else{
+      throw std::runtime_error("TMVA::SOFIE ONNX Parser Softmax op has input tensor" + input_name + " but its type is not yet registered");
+   }
+
+   std::unique_ptr<ROperator> op;
+
+
+   switch(input_type){
+   case ETensorType::FLOAT:
+      op.reset(new ROperator_Softmax<float>(nodeproto.input(0), nodeproto.output(0)));
+      break;
+   default:
+      throw std::runtime_error("TMVA::SOFIE - Unsupported - Operator Softmax does not yet support input type " + std::to_string(static_cast<int>(input_type)));
+   }
+
+   ETensorType output_type = (op->TypeInference({input_type}))[0];
+   auto it2 = tensor_type.find(nodeproto.output(0));
+   if (it2 == tensor_type.end()){
+      tensor_type[nodeproto.output(0)] = output_type;
+   }
+
+   return op;
+}
+
+std::unique_ptr<ROperator> make_ROperator_MatMul(const onnx::NodeProto& nodeproto, const onnx::GraphProto& /*graphproto */, std::unordered_map<std::string, ETensorType>& tensor_type){
+
+   ETensorType input_type = ETensorType::UNDEFINED;
+
+   for (int i = 0; i < 2; ++i) {
+      auto input_name = nodeproto.input(i);
+      auto it = tensor_type.find(input_name);
+      if (it != tensor_type.end()){
+         // according to ONNX both inputs have same time
+         if (i == 0) input_type = it->second;
+         else
+            assert(it->second == input_type);
+      } else {
+         throw std::runtime_error("TMVA::SOFIE ONNX Parser MatMul op has input tensor" + input_name + " but its type is not yet registered");
+      }
+   }
+
+   std::unique_ptr<ROperator> op;
+
+   switch(input_type){
+   case ETensorType::FLOAT:
+      if (nodeproto.input_size() == 2){
+         op.reset(new ROperator_Gemm<float>(attr_alpha, attr_beta, attr_transA, attr_transB, nodeproto.input(0), nodeproto.input(1), nodeproto.output(0)));
+      }else{
+         op.reset(new ROperator_Gemm<float>(attr_alpha, attr_beta, attr_transA, attr_transB, nodeproto.input(0), nodeproto.input(1), nodeproto.input(2), nodeproto.output(0)));
+      }
+      break;
+   default:
+      throw std::runtime_error("TMVA::SOFIE - Unsupported - Operator MatMul does not yet support input type " + std::to_string(static_cast<int>(input_type)));
    }
 
    ETensorType output_type = (op->TypeInference({input_type}))[0];
@@ -1046,7 +1113,7 @@ RModel RModelParser_ONNX::Parse(std::string filename){
          rmodel.AddBlasRoutines({"Gemm", "Axpy"});
       } else if (op_type == "RNN") {
          rmodel.AddBlasRoutines({"Gemm", "Axpy"});
-      } else if (op_type == "Selu" || op_type == "Sigmoid") {
+      } else if (op_type == "Selu" || op_type == "Sigmoid" || op_type == "LeakyRelu") {
          rmodel.AddNeededStdLib("cmath");
       } else if (op_type == "LSTM") {
          rmodel.AddBlasRoutines({"Gemm", "Axpy"});
